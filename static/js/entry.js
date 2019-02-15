58cd1f1397dd68d6025d4ae5effe10241db09cbd
yo16 <>
2019-02-15 23:30:41 +0900

    shaffle users

--- static/js/entry.js
+++ static/js/entry.js
@@ -1,10 +1,30 @@
 /*
 entry.js
 
-
 2019/2/7 y.ikeda
 */
 
 $(document).ready(function(){
+    $("#btn_regist_stayorder").click(function(){
+        regist();
+    });
+    $("#btn_regist_reorder").click(function(){
+        // ランダムで順序を入れ替える
+        var cur_members = $("#area_players").val().split(/[\r\n]+/);
+        for(var i = cur_members.length - 1; i > 0; i--){
+            var r = Math.floor(Math.random() * (i + 1));
+            var tmp = cur_members[i];
+            cur_members[i] = cur_members[r];
+            cur_members[r] = tmp;
+        }
+        $("#area_players").val(cur_members.join("\n"));
 
+        regist();
+    });
 });
+
+
+function regist(){
+    var cur_members = $("#area_players").val().split(/[\r\n]+/);
+    
+}
